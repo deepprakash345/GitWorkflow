8695147d42d47474c5f8b7c183b691d359d837ba
vdua <vdua@adobe.com>
2021-08-19 22:36:21 +0530

    CQ-4330000 adding checkbox component

---
+++ packages/forms-next-react-core-components/src/components/Checkbox.tsx
@@ -0,0 +1,31 @@
+import {Checkbox} from '@adobe/react-spectrum';
+import {FieldJson} from '@adobe/forms-next-core';
+import {useRuleEngine} from '../react-mapper/hooks';
+import {SpectrumCheckboxProps} from '@react-types/checkbox';
+const CheckboxComponent = function (originalProps: FieldJson) {
+    const [props, dispatchChange] = useRuleEngine<FieldJson, string>(originalProps);
+    console.log('rendering TextField ' + props[':id'] + ' ' + props[':value']);
+    const title = props[':hideTitle'] === true ? '' :props[':title'];
+    const value = props[':value'];
+    const selectedValue = props[':constraints']?.[':options']?.[0]?.[':value'];
+    const isRequired = props[':constraints']?.[':required'];
+    const spectrumProps: SpectrumCheckboxProps = {
+        name: props[':name'],
+        value: props[':value'] as string,
+        isSelected: value === selectedValue,
+        isRequired,
+        validationState: props[':valid'] === false ? 'invalid' : (props[':valid'] === undefined ? undefined : 'valid')
+    };
+
+    const handleChange = (isSelected: boolean) => {
+        const value = isSelected ? props[':constraints']?.[':options']?.[0]?.[':value'] : undefined;
+        dispatchChange(value);
+    };
+
+    return (
+        <Checkbox {...spectrumProps} onChange={handleChange}>{title}</Checkbox>
+    );
+};
+
+
+export default CheckboxComponent;
--- packages/forms-next-react-core-components/src/mappings.ts
+++ packages/forms-next-react-core-components/src/mappings.ts
@@ -1,8 +1,10 @@
 import TextField from './components/TextField';
 import Button from './components/Button';
 import Panel from './components/Panel';
+import Checkbox from './components/Checkbox';
 const mappings: any = {
     'fd/af/components/guidetextbox' : TextField,
+    'fd/af/components/guidecheckbox' : Checkbox,
     'fd/af/components/guidebutton' : Button,
     'fd/af/components/rootPanel' : Panel
 };
