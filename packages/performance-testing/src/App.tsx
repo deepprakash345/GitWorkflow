f3999efb23b16f9e3ab212498c17ed25f7839f79
vdua <vdua@adobe.com>
2022-03-01 14:13:13 +0530

    fixing imports after renaming

--- packages/performance-testing/src/App.tsx
+++ packages/performance-testing/src/App.tsx
@@ -5,7 +5,7 @@ import { mappings } from "@aemforms/crispr-react-core-components";
 // @ts-ignore
 import { AdaptiveForm } from "@aemforms/crispr-react-bindings";
 // import formJson from "./JSON/form.json";
-import formJson from "./JSON/smallForm.json";
+import formJson from "./JSON/smallForm.form.json";
 
 
 const App = () => {
